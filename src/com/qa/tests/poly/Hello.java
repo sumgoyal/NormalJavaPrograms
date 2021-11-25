a480e17317b0905c623b3b4a495df46c9b760d89
sumgoyal <sumitgoyal0927@gmail.com>
2021-11-25 13:26:38 +0530

    added hello v3

--- src/com/qa/tests/poly/Hello.java
+++ src/com/qa/tests/poly/Hello.java
@@ -5,6 +5,11 @@ public class Hello {
 	public static void main(String[] args) {
 
 		System.out.println("hello world");
+		System.out.println("hello worldv2");
+		System.out.println("hello world v3");
+
+
+		
 	}
 
 }
